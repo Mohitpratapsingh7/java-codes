718ce3d6650f9fd7c68d1b1afff64b9e952b5656
Mohitpratapsingh7 <mohitpratapsingh044@gmail.com>
2025-11-19 11:27:23 +0530

    Update main method to print new names

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("kaushal");
-        System.out.println("kuntal");
-        System.out.println("kASHUU");
+        System.out.println("mohit");
+        System.out.println("pratap ");
+        System.out.println("singh");
     }
+
 }  
